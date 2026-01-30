8cf768542d07acb44fb4a1dc78d74003d7c6df7f
Musab Al-Zoubi <126392782+MusabAlzoubi@users.noreply.github.com>
2026-01-31 01:03:32 +0300

    Fix Spatie permission middleware aliasing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $spatieAliases = [
+            'Spatie\\Permission\\Middlewares\\RoleMiddleware' => 'Spatie\\Permission\\Middleware\\RoleMiddleware',
+            'Spatie\\Permission\\Middlewares\\PermissionMiddleware' => 'Spatie\\Permission\\Middleware\\PermissionMiddleware',
+            'Spatie\\Permission\\Middlewares\\RoleOrPermissionMiddleware' => 'Spatie\\Permission\\Middleware\\RoleOrPermissionMiddleware',
+        ];
+
+        foreach ($spatieAliases as $expected => $actual) {
+            if (!class_exists($expected) && class_exists($actual)) {
+                class_alias($actual, $expected);
+            }
+        }
     }
 }
