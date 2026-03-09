4bc1cf411779dd971b1101ec8be7a76d36b42006
mays-alkhalil <maysalkhalil02@gmail.com>
2026-03-09 13:21:15 +0300

    Refactor transport sidebar and lock admin-only transport sections

--- resources/views/pages/transport/partials/sidebar.blade.php
+++ resources/views/pages/transport/partials/sidebar.blade.php
@@ -1,31 +1,31 @@
-<li class="nxl-item">
-    <a class="nxl-link {{ request()->routeIs('role.transport.vehicles.*') ? 'active' : '' }}" href="{{ route('role.transport.vehicles.index') }}">
-        <span class="nxl-micon"><i class="feather-truck"></i></span>
-        <span class="nxl-mtext">{{ __('app.roles.transport.vehicles.title') }}</span>
-    </a>
-</li>
-<li class="nxl-item">
-    <a class="nxl-link {{ request()->routeIs('role.transport.drivers.*') ? 'active' : '' }}" href="{{ route('role.transport.drivers.index') }}">
-        <span class="nxl-micon"><i class="feather-user"></i></span>
-        <span class="nxl-mtext">{{ __('app.roles.transport.drivers.title') }}</span>
-    </a>
-</li>
-<li class="nxl-item">
-    <a class="nxl-link {{ request()->routeIs('role.transport.trips.*') ? 'active' : '' }}" href="{{ route('role.transport.trips.index') }}">
-        <span class="nxl-micon"><i class="feather-navigation"></i></span>
-        <span class="nxl-mtext">{{ __('app.roles.transport.trips.title') }}</span>
-    </a>
-</li>
-<li class="nxl-item">
-    <a class="nxl-link {{ request()->routeIs('role.transport.requests.*') ? 'active' : '' }}" href="{{ route('role.transport.requests.index') }}">
-        <span class="nxl-micon"><i class="feather-file-text"></i></span>
-        <span class="nxl-mtext">{{ __('app.roles.transport.requests.title') }}</span>
-    </a>
-</li>
+@php
+    $transportRoutesActive = request()->routeIs('role.transport.*');
+@endphp
 
-<li class="nxl-item">
-    <a class="nxl-link {{ request()->routeIs('role.transport.movements.*') ? 'active' : '' }}" href="{{ route('role.transport.movements.index') }}">
-        <span class="nxl-micon"><i class="feather-map-pin"></i></span>
-        <span class="nxl-mtext">{{ __('app.roles.transport.movements.title') }}</span>
+<li class="nxl-item nxl-hasmenu {{ $transportRoutesActive ? 'active' : '' }}">
+    <a href="javascript:void(0);" class="nxl-link {{ $transportRoutesActive ? 'active' : '' }}">
+        <span class="nxl-micon"><i class="feather-truck"></i></span>
+        <span class="nxl-mtext">{{ __('app.acl.permissions.transport_view') }}</span><span class="nxl-arrow"><i class="feather-chevron-right"></i></span>
     </a>
+    <ul class="nxl-submenu">
+        @role('transport_officer')
+            <li class="nxl-item">
+                <a class="nxl-link {{ request()->routeIs('role.transport.vehicles.*') ? 'active' : '' }}" href="{{ route('role.transport.vehicles.index') }}">{{ __('app.roles.transport.vehicles.title') }}</a>
+            </li>
+            <li class="nxl-item">
+                <a class="nxl-link {{ request()->routeIs('role.transport.drivers.*') ? 'active' : '' }}" href="{{ route('role.transport.drivers.index') }}">{{ __('app.roles.transport.drivers.title') }}</a>
+            </li>
+            <li class="nxl-item">
+                <a class="nxl-link {{ request()->routeIs('role.transport.trips.*') ? 'active' : '' }}" href="{{ route('role.transport.trips.index') }}">{{ __('app.roles.transport.trips.title') }}</a>
+            </li>
+        @endrole
+        @role('super_admin')
+            <li class="nxl-item">
+                <a class="nxl-link {{ request()->routeIs('role.transport.requests.*') ? 'active' : '' }}" href="{{ route('role.transport.requests.index') }}">{{ __('app.roles.transport.requests.title') }}</a>
+            </li>
+            <li class="nxl-item">
+                <a class="nxl-link {{ request()->routeIs('role.transport.movements.*') ? 'active' : '' }}" href="{{ route('role.transport.movements.index') }}">{{ __('app.roles.transport.movements.title') }}</a>
+            </li>
+        @endrole
+    </ul>
 </li>
